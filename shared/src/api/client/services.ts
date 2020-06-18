4c98603c4f5398dc8c433563337c2e880c5ce797
Simon <twop.sk@gmail.com>
2020-06-17 19:54:18 -0700

    WIP: sketch of moving hover provider registry to the web worker

--- shared/src/api/client/services.ts
+++ shared/src/api/client/services.ts
@@ -7,7 +7,6 @@ import { ContributionRegistry } from './services/contribution'
 import { TextDocumentDecorationProviderRegistry } from './services/decoration'
 import { createViewerService } from './services/viewerService'
 import { ExtensionsService } from './services/extensionsService'
-import { TextDocumentHoverProviderRegistry } from './services/hover'
 import { LinkPreviewProviderRegistry } from './services/linkPreview'
 import { TextDocumentLocationProviderIDRegistry, TextDocumentLocationProviderRegistry } from './services/location'
 import { createModelService } from './services/modelService'
@@ -50,7 +49,7 @@ export class Services {
     public readonly textDocumentDefinition = new TextDocumentLocationProviderRegistry()
     public readonly textDocumentReferences = new TextDocumentLocationProviderRegistry<ReferenceParams>()
     public readonly textDocumentLocations = new TextDocumentLocationProviderIDRegistry()
-    public readonly textDocumentHover = new TextDocumentHoverProviderRegistry()
+    public readonly textDocumentHover = {}
     public readonly textDocumentDecoration = new TextDocumentDecorationProviderRegistry()
     public readonly queryTransformer: { transformQuery?: (query: string) => Observable<string> } = {}
     public readonly panelViews = new PanelViewProviderRegistry()
